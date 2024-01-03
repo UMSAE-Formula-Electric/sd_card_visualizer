e30baf427786b18b5f5f066dd1cbaaf0ca2f7392
niko <niko@nikos-thinkpad>
2024-01-02 20:03:25 -0600

    Tying out a new approach to the slider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-import MultiRangeSlider from "multi-range-slider-react";
+// import MultiRangeSlider from "multi-range-slider-react";
 import {
   select,
   line,
@@ -18,6 +18,7 @@ function LineChart({data}) {
     
     const [xs, setXs] = useState(data); // the list of points to graph
 
+    /*
     const [min, setMin] = useState(0); // the smallest time value (x)
     const [max, setMax] = useState(1); // the largest time value (x)
 
@@ -25,11 +26,13 @@ function LineChart({data}) {
     const [maxValue, setMaxValue] = useState(1); // the upper bound of our viewing window
 
     const [disabled, setDisabled] = useState(false); // managing following state
+    */
 
     const [time, setTime] = useState(0);
 
     const svgRef = useRef(); // ref for the d3 graph
 
+    /*
     const handleInput = (e) => {
 	setMinValue(e.minValue);
 	if(!disabled) {
@@ -40,22 +43,19 @@ function LineChart({data}) {
 	    setDisabled(false); 
 	}
     };
+    */
 
     // Modifies the svgRef with the graph in view
     const drawChart = () => {
 
 	// Idk what alot of this does, it was copied from somewhere
-	
-	const xsInView = xs.filter(p => {
-	    return p['x'] >= minValue && p['x'] <= maxValue;
-	});
-
 	const svg = select(svgRef.current);
 
 	//scales
 	const xScale = scaleLinear()
-	    .domain([0, xsInView.length - 1]) // this allows values to fill graph (more values more spread out)
-	    .range([0, 1000]);
+	    //.domain([0, xs.length - 1]) // this allows values to fill graph (more values more spread out)
+	    //.domain([0, xs.length - 1])
+	    //.range([0, 1000]);
 
 	const yScale = scaleLinear()
 	      .domain([0, 100])
@@ -82,8 +82,9 @@ function LineChart({data}) {
 
 	//drawing the line
 	svg
+	    .style("width", xs.length + "px")
 	    .selectAll(".line")
-	    .data([xsInView])
+	    .data([xs])
 	    .join("path")
 	    .attr("class", "line")
 	    .attr("d", myLine)
@@ -95,7 +96,7 @@ function LineChart({data}) {
     useEffect(() => {
 	const interval = setInterval(() => {
 	    updateGraph();
-	}, 250);
+	}, 12);
 
 	// find the minimum and maximum time values (x)
 	xs.sort(p => p['x']);
@@ -103,6 +104,7 @@ function LineChart({data}) {
 	let newMax = Math.max(...[...xs.map(p => p['x']), 1]); // max or 1
 	let newMin = Math.min(...[...xs.map(p => p['x']), 0]); // min or 0
 
+	/*
 	setMax(newMax);
 	setMin(newMin);
 
@@ -111,11 +113,12 @@ function LineChart({data}) {
 	    setMaxValue(newMax);
 	    setDisabled(true);
 	}
+	*/
 
 	drawChart(); // Redraw chart with new metrics
 
 	return () => clearInterval(interval);
-    }, [xs, minValue, maxValue]);
+    }, [xs]);
 
     // Creates a new point on the graph and increments time
     function updateGraph() {
@@ -131,9 +134,10 @@ function LineChart({data}) {
 
     return (
 	<div>
-	    <div style={{padding: "5vw"}}>
-		<svg ref={svgRef} style={{border: "1px solid black", width: "100%"}}/>
+	    <div style={{width: "100%", border: "1px solid black", overflowY: "scroll"}}>
+		<svg ref={svgRef} style={{width: "100%"}}/>
 	    </div>
+	    {/*
 	    <MultiRangeSlider
 		    min={min} // current min value set by user
 		    max={max} // current max value set by user
@@ -143,6 +147,7 @@ function LineChart({data}) {
 		    maxValue={maxValue} // maximum value of the slider
 		    onInput={(e) => handleInput(e)}
 	    />
+	    */}
 	    <button onClick={updateGraph}>Update</button>
 	</div>
     );
