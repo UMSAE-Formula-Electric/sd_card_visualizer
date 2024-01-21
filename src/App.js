82af376de8e6e15da7c317b1763bd685fe0672e5
niko <niko@nikos-thinkpad>
2024-01-21 12:39:44 -0600

    Complete mess

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useRef, useEffect, useState } from "react";
 
+import * as d3 from "d3";
+
+/*
 import {
   select,
   line,
@@ -7,7 +10,11 @@ import {
   scaleLinear,
   axisBottom,
   axisLeft,
+  range,
+  zoom,
+  event,
 } from "d3";
+*/
 
 function LineChart({data}) {
 
@@ -28,6 +35,7 @@ function LineChart({data}) {
     const svgRef = useRef(); // ref for the d3 graph
 
     // Modifies the svgRef with the graph in view
+    /*
     const drawChart = () => {
 
 	// Idk what alot of this does, it was copied from somewhere
@@ -75,14 +83,83 @@ function LineChart({data}) {
 	    .attr("fill", "none")
 	    .attr("stroke", "#00bfa6");
     }
+    // https://gist.github.com/mbostock/1642874
+    */
+
+    const drawChart = () => {
+
+	// Idk what alot of this does, it was copied from somewhere
+	const svg = d3.select(svgRef.current);
+
+	//scales
+
+	const chartWidth = document.getElementById("graph").scrollWidth;
+	const xsInView = xs.length * (sliderValue / 100);
+
+	const xScale = d3.scaleLinear();
+	      //.domain([0, chartWidth])
+	      //.range([0, chartWidth / (sliderValue / 100)]);
+	      //.domain([Math.min(...xs.map((p) => p.x)), Math.max(...xs.map((p) => p.x))])
+	      //.range([0, xsInView]);
+
+	const yScale = d3.scaleLinear();
+	    //.domain([0, time])
+	      //.range([0, sliderValue]);
+	
+
+	/*
+	const xScale = scaleLinear()
+	      .domain([0, xsInView]) // this allows values to fill graph (more values more spread out)
+	      .range([0, time]);
+
+	const yScale = scaleLinear()
+	      .domain([0, 100])
+	      .range([100, 0]);
+	*/
+
+	//axes
+	const xAxis = d3.axisBottom(xScale).ticks(xs.length);
+	svg
+	    .select(".x-axis")
+	    .style("transform", "translateY(100px)")
+	    .call(xAxis);
+
+	const yAxis = d3.axisLeft(yScale);
+	svg
+	    .select(".y-axis")
+	    .style("transform", "translateX(0px)")
+	    .call(yAxis);
+
+	//line generator
+	const myLine = d3.line()
+	    .x((d, i) => xScale(i))
+	    .y((d) => yScale(d.y))
+	    .curve(d3.curveCardinal);
+
+	//drawing the line
+	svg
+	    .style("width", xs.length + "px")
+	    .call(d3.zoom().on("zoom", (event) => {   // <-- `event` argument
+		svg.attr("transform", event.transform); // <-- use `event` here
+	    }))
+	    .selectAll(".line")
+	    .data([xs])
+	    .join("path")
+	    .attr("class", "line")
+	    .attr("d", myLine)
+	    .attr("fill", "none")
+	    .attr("stroke", "#00bfa6");
+    }
 
     // Called everytime we change our time window or add new data points
     useEffect(() => {
 	const interval = setInterval(() => {
 	    updateGraph();
-	}, 12);
+	}, 10);
 
-	drawChart(); // Redraw chart with new metrics
+	//drawChart(xs[xs.length - 1]); // Redraw chart with new metrics
+	//drawChart2(xs);
+	drawChart();
 
 	// Snaps graph to newest value
 	if(follow) {
@@ -98,7 +175,7 @@ function LineChart({data}) {
 	const point = {
 	    x: time, 
 	    y: Math.random() * 100,
-	    //y: 10 * Math.cos(time)//Math.random() * 100,
+	    //y: 10 * Math.cos(time)
 	};
 
 	setTime(time + 1); 
@@ -109,7 +186,7 @@ function LineChart({data}) {
     return (
 	<div>
 	    <div style={{width: "100%", border: "1px solid black", overflowY: "scroll"}} id="graph">
-		<svg ref={svgRef} style={{width: "100%"}}/>
+		<svg ref={svgRef} style={{width: "100%"}} id="svg"/>
 	    </div>
 	    {/* This slider below will allow you to select the range of your time */}
 	    <div sytyle={{width: "100%"}}>
